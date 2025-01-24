303135e4b1a511d7ee8a1de7a58f6b0771877837
himore <p1199019@naver.com>
2025-01-24 17:40:49 +0900

    1

---
+++ Day01/dom/dom.js
@@ -0,0 +1,29 @@
+// const h1 = document.createElement("h1");
+// h1.innerText = "고기싸롱 어서오고";
+// h1.style.color = "red";
+// document.body.appendChild(h1);
+
+// const article = document.createElement("article");
+// const h1 = document.createElement("h1");
+// const btn = document.createElement("button");
+
+// const meats = [
+//   "삼겹살",
+//   "목살",
+//   "돼지갈비",
+//   "양념갈비",
+//   "항정살",
+//   "갈매기살",
+//   "꼬들살",
+//   "냉삼",
+//   "뽈살",
+//   "돼지껍데기",
+// ];
+
+// meats.forEach((x, i) => {
+//   const btn = document.createElement("button");
+//   btn.style.backgroundColor = i % 2 ? "skyblue" : "yellow";
+//   btn.innerText = x;
+//   btn.addEventListener("click", () => alert(`${x}고기`));
+//   document.body.appendChild(btn);
+// });
---
+++ Day01/event/index.js
@@ -0,0 +1,4 @@
+//event -> click이벤트, input이벤트, scroll이벤트
+
+const hello = () => alert("수고하세요");
+const bye = () => alert("ㅂㅂ");
